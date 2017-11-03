f96a7aace4f8e1b60a5e843f24540a5937de2c11
rajatjasuja23 <rajat121jasuja@gmail.com>
2017-11-03 12:44:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+// import { Provider } from 'react-redux'
+// import { createStore} from 'redux'
+// import todoApp from './reducers';
+// import Allreducers from './reducers/Todoreducers/Allreducers.js';
+
+
+// let store = createStore(Allreducers);
+
+// ReactDOM.render( <Provider store={store}>
+//                     <App />
+//                 </Provider>, document.getElementById('app'));
+
+ ReactDOM.render( <App/>
+, document.getElementById('app'));
